5c6d687bca1e97f0ac575e7d70a5eef5af44fd95
Kalvin Manson <kalvinmanson@gmail.com>
2018-12-12 21:31:01 -0500

    Buscador que se oculta

--- header.php
+++ header.php
@@ -60,6 +60,7 @@
 
   		<img src="assets/img/logo.png" class="logo">
   		<div class="menucar">
+  			<a href="#" id="trBuscar" style="display: none;"><i class="fa fa-search"></i></a>
   			<a href="#"><span class="boxcar2">02</span></a>
   		</div>
   	</div>
@@ -68,153 +69,153 @@
 	<div class="collapse navbar-collapse js-navbar-collapse">
 		<ul class="nav navbar-nav">
 			<li class="dropdown mega-dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Cuidado en casa <br>&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Cuidado en casa <br>&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Camas</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Movilidad</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Seguridad en el baño</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Incontinencia</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Camas</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Movilidad</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Seguridad en el baño</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Incontinencia</a></li>
 						</ul>
 					</li>
 					<li class="col-md-2  col-sm-3">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-6.png" class="icon-menu"> Accesorios y ayuda</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-7.png" class="icon-menu"> Pastillero y partidores</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-8.png" class="icon-menu"> Patos y orinales</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-9.png" class="icon-menu"> Sillas y ayudas sanitarias</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-6.png" class="icon-menu"> Accesorios y ayuda</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-7.png" class="icon-menu"> Pastillero y partidores</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-8.png" class="icon-menu"> Patos y orinales</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-9.png" class="icon-menu"> Sillas y ayudas sanitarias</a></li>
 						</ul>
 					</li>
 					<li class="col-md-2  col-sm-3">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-10.png" class="icon-menu"> Almohadas y cojines</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-10.png" class="icon-menu"> Almohadas y cojines</a></li>
 						</ul>
 					</li>
 				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Deporte<br>&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>						
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Deporte<br>&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Citas y Vendajes</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Analgesia</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Línea Infantil</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Fitness</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Citas y Vendajes</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Analgesia</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Línea Infantil</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Fitness</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Prevención y <br>
-cuidado de várices <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+cuidado de várices <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-4  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Compresión Baja 8-15 MMHG</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Compresión Media 15-20 MMHG</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Compresión Firme 20-30 MMHG</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Compresión Extra-Firme 30-40 MMHG</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Compresión Baja 8-15 MMHG</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Compresión Media 15-20 MMHG</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Compresión Firme 20-30 MMHG</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Compresión Extra-Firme 30-40 MMHG</a></li>
 						</ul>
 					</li>
 					<li class="col-md-2 col-sm-4 ">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Medias Antiembólicas 18 MMHG</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Línea Deportiva </a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Medias Antiembólicas 18 MMHG</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Línea Deportiva </a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Cuidado<br>
-del cuerpo <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+del cuerpo <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Extremidades Superiores</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Extremidades Inferiores</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Pie</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Suplementos Dietarios</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Extremidades Superiores</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Extremidades Inferiores</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Pie</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Suplementos Dietarios</a></li>
 						</ul>
 					</li>
 
 					<li class="col-md-2 col-sm-2">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Cuello</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Espalda</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Piel</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Ocular</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Cuello</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Espalda</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Piel</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Ocular</a></li>
 						</ul>
 					</li>
 					<li class="col-md-2 col-sm-2">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Auditivo</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Fajas</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Auditivo</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Fajas</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Equipo de <br>
-diagnóstico <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+diagnóstico <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Estetoscopio</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Tensiómetros</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Monitoreo</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Glucometría</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Estetoscopio</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Tensiómetros</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Monitoreo</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Glucometría</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Médico<br>
-quirúrgico <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+quirúrgico <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Primeros Auxilios</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Material Hospitalario</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Primeros Auxilios</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Material Hospitalario</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Terapia<br>
-&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Equipos de Fisioterapia</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Aspiradoras Y Succionadoras</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Cilindros de Oxígeno</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Concentradores</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Equipos de Fisioterapia</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Aspiradoras Y Succionadoras</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-2.png" class="icon-menu"> Cilindros de Oxígeno</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-3.png" class="icon-menu"> Concentradores</a></li>
 						</ul>
 					</li>
 
 					<li class="col-md-2 col-sm-2">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Humidificadores</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Nebulizadores</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Máquinas CPAP</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Vaporizadores</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Humidificadores</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Nebulizadores</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Máquinas CPAP</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Vaporizadores</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 			<li class="dropdown mega-dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">¡OFERTAS!<br>
-&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>				
+&nbsp;<div class="icon-sub"><img src="assets/img/icon/angle-down.png"></div></a>
 				<ul class="dropdown-menu mega-dropdown-menu">
 					<li class="col-md-2 col-md-offset-3  col-sm-3  col-sm-offset-1">
 						<ul>
-							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Liquidación</a></li> 
-							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Promociones</a></li> 
+							<li><a href="#"><img src="assets/img/icon/icon-1.png" class="icon-menu"> Liquidación</a></li>
+							<li><a href="#"><img src="assets/img/icon/icon-5.png" class="icon-menu"> Promociones</a></li>
 						</ul>
 					</li>
-				</ul>				
+				</ul>
 			</li>
 
 			<li class="dropdown mega-dropdown">
@@ -225,12 +226,10 @@ quirúrgico <div class="icon-sub"><img src="assets/img/icon/angle-down.png"></di
               </div>
 			</li>
 		</ul>
- 
+
 	</div><!-- /.nav-collapse -->
   </nav>
 
- <div id="bucas" class="busc2">
-        <input type="search" name="nombre" placeholder="Qué producto buscas?">
-</div>
-
-
+	<div id="bucas" class="busc2">
+		<input type="search" id="inputBuscar" name="nombre" placeholder="Qué producto buscas?">
+	</div>
